df6d5c3d43fdfb097ae3638be3162933cc1a7f3c
unknown <hello.jewelahmed@gmail.com>
2021-03-16 09:18:43 +0600

    truthy-falsy value added

---
+++ truth-false.js
@@ -0,0 +1,23 @@
+// Falsy Value
+// ""
+// 0
+// Undefined
+// null
+// NaN
+
+// Truthy Value
+// " "
+// "anything"
+// []
+// {object}
+
+
+
+let name = {} 
+console.log(name);
+if(name){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
