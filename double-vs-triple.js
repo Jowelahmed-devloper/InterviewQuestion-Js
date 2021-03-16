4f4304dff5352bf8fa5911c8f975558c4f7c4e24
unknown <hello.jewelahmed@gmail.com>
2021-03-16 13:32:17 +0600

    double equal vs triple equal

---
+++ double-vs-triple.js
@@ -0,0 +1,21 @@
+// const first = 2;
+// const second = "2";
+
+// if(first === second){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+const first = 0;
+const second = false;
+if(first === second){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
+
+// Important note
+// == (double equal) only value ta ke check kore. and ===(triple equal) value ebong value ta ki type a ace dutoi check kore output dey. example = "2" 2 is string
