9318c1159a80f0e12ec3b7ed50ff73708e84e1c4
unknown <hello.jewelahmed@gmail.com>
2021-03-16 09:49:22 +0600

    undefined added

---
+++ null.js
@@ -0,0 +1,25 @@
+Finding Undefined Value
+
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1+ num2);
+    // return num1 + num2;
+}
+const result = add(5,8);
+console.log(result);
+
+function add (num1, num2){
+    return num1+ num2;
+}
+const result = add(11)
+console.log(result);
+
+const premik = {name:"Jowel", phone: 01714-843167}
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+
